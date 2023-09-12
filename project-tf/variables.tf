5f48f30837368a3ab703231c747d1b3492b9a40d
edt-356 <edt356@gmail.com>
2023-09-12 14:19:53 -0500

    .

--- project-tf/variables.tf
+++ project-tf/variables.tf
@@ -9,7 +9,7 @@ variable "container_name_from_root" {
 }
 
 # used in "my_modules/container_maker/main.tf" and may be remapped in the root "main.tf"
-variable "external_port" {
+variable "external_port_from_root" {
   description = "External port on the container"
   type        = number
   default     = 2222
